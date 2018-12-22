384c116028b00b9225774737db439a243edc07c1
Gabriel Osborne <osbornegabriel@gmail.com>
2018-12-22 14:04:20 -0500

    Add length variables

--- phraseomatic.java
+++ phraseomatic.java
@@ -1,7 +1,14 @@
 public class PhraseOMatic {
   public static void main (String[] args) {
     String[] wordListOne = { "Fight", " Club", "multi-tier", "7 days a week", "more words", "blah blah"};
-    String[] worldListTwo = {"Awesome", "Empowered", "So Nice", "Listing", "List List", "Yay!"};
-    String wordListThree = {"Process", " tippingpoint", "Creative", "saturday", "java", "java java", "java java java"};
+    String[] wordListTwo = {"Awesome", "Empowered", "So Nice", "Listing", "List List", "Yay!"};
+    String[] wordListThree = {"Process", " tippingpoint", "Creative", "saturday", "java", "java java", "java java java"};
+
+    int oneLength = wordListOne.length;
+    int twoLength = wordListTwo.length;
+    int threeLength = wordListThree.length;
+
   }
+
+
 }
