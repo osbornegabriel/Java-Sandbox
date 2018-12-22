b0ea4cdd2c236bbe9e16266c6eae51e5e1799d77
Gabriel Osborne <osbornegabriel@gmail.com>
2018-12-22 13:56:37 -0500

    create word lists"

---
+++ phraseomatic.java
@@ -0,0 +1,7 @@
+public class PhraseOMatic {
+  public static void main (String[] args) {
+    String[] wordListOne = { "Fight", " Club", "multi-tier", "7 days a week", "more words", "blah blah"};
+    String[] worldListTwo = {"Awesome", "Empowered", "So Nice", "Listing", "List List", "Yay!"};
+    String wordListThree = {"Process", " tippingpoint", "Creative", "saturday", "java", "java java", "java java java"};
+  }
+}
